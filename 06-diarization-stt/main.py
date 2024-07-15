1eddb82f29d3f4fcbd43ed333d3297194896699a
Anna E. Molosky <856929+amolosky@users.noreply.github.com>
2024-07-15 17:47:07 -0400

    Uploading diarization and more

---
+++ 06-diarization-stt/main.py
@@ -0,0 +1,28 @@
+# Start by making sure the `assemblyai` package is installed.
+# If not, you can install it by running the following command:
+# pip install -U assemblyai
+#
+# Note: Some macOS users may need to use `pip3` instead of `pip`.
+
+import assemblyai as aai
+
+# Replace with your API key
+aai.settings.api_key = "ce2e5c0483264ca084eaa529e2311c4c"
+
+# URL of the file to transcribe
+FILE_URL = '06-diarization-stt/American-life-vacations.mp3' 
+
+# You can also transcribe a local file by passing in a file path
+# FILE_URL = './path/to/file.mp3'
+
+config = aai.TranscriptionConfig(
+  speaker_labels=True,speakers_expected=6,format_text=True)
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe(
+  FILE_URL,
+  config=config
+)
+
+for utterance in transcript.utterances:
+  print(f"Speaker {utterance.speaker}: {utterance.text}")
